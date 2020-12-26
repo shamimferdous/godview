5f559fa293bcf6818ee76e08af72506aee95d2f4
Shamim Ferdous <shamimferdous5@gmail.com>
2020-12-26 15:58:45 +0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,88 @@
 import logo from './logo.svg';
 import './App.css';
+import GoogleMap from 'google-map-react';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import { Demo } from './demo';
+import { usePosition } from './custom-hooks/usePosition';
 
-function App() {
+function App({ watch, settings }) {
+
+  const [center, setCenter] = useState({
+    lat: 59.95,
+    lng: 30.33
+  });
+
+  const [zoom, setZoom] = useState(11);
+
+  const [marker, setMarker] = useState({
+    lat: 23.955413,
+    lng: 90.337844,
+  })
+
+
+  const {
+    latitude,
+    longitude,
+    timestamp,
+    accuracy,
+    speed,
+    error,
+  } = usePosition(watch, settings);
+
+
+  useEffect(() => {
+
+    console.log(`Timestamp was changed`);
+
+    // navigator?.geolocation.getCurrentPosition(({ coords: { latitude: lat, longitude: lng } }) => {
+    //   const pos = { lat, lng }
+    //   console.log(pos);
+
+    //   setMarker({
+    //     lat: pos.lat,
+    //     lng: pos.lng
+    //   })
+    // })
+
+  }, [timestamp]);
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div style={{ display: 'flex' }}>
+
+      <div style={{ flex: '0 0 50%' }}>
+        <Demo watch />
+
+      </div>
+
+      <div style={{ height: '100vh', width: '100%', flex: '0 0 50%' }}>
+        <GoogleMap
+          bootstrapURLKeys={{ key: 'AIzaSyCvv_c6oudIDXsjU5bVR7Y3uL_2mK1NcfU' }}
+          //defaultCenter={center}
+          defaultZoom={zoom}
+          center={marker}
         >
-          Learn React
-        </a>
-      </header>
+          <AnyReactComponent
+            //lat={marker.lat}
+            //lng={marker.lng}
+            lat={latitude}
+            lng={longitude}
+            text="My Marker Fucking Marker"
+          />
+        </GoogleMap>
+      </div>
+
+
     </div>
+
   );
 }
 
 export default App;
+
+
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
+
+
---
+++ src/demo.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { usePosition } from './custom-hooks/usePosition';
+
+export const Demo = ({ watch, settings }) => {
+    const {
+        latitude,
+        longitude,
+        timestamp,
+        accuracy,
+        speed,
+        error,
+    } = usePosition(watch, settings);
+
+    const loader = !latitude && !error ? (
+        <>
+            <div>Trying to fetch location...</div>
+            <br />
+        </>
+    ) : null;
+
+    return (
+        <>
+            {loader}
+            <code>
+                latitude: {latitude}<br />
+                longitude: {longitude}<br />
+                timestamp: {timestamp}<br />
+                accuracy: {accuracy && `${accuracy}m`}<br />
+                speed: {speed}<br />
+                error: {error}
+            </code>
+        </>
+    );
+};
+
+Demo.propTypes = {
+    watch: PropTypes.bool,
+    settings: PropTypes.object,
+};
